7dd79fa364eafa19087de203eae64c847d780deb
BogdanFedorniak <91177826+BogdanFedorniak@users.noreply.github.com>
2024-03-06 00:03:59 +0200

    Update SpeakHello.js

--- lab2/SpeakHello.js
+++ lab2/SpeakHello.js
@@ -1,7 +1,9 @@
-var speakWord = "Hello";
-
-
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+(function(window) {
+    var speakWord = "Hello";
+    var helloSpeaker = {};
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
 
