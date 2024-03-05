38bb4d4b95a5abbd4d09580a306112b40eb092cd
BogdanFedorniak <91177826+BogdanFedorniak@users.noreply.github.com>
2024-03-06 00:50:00 +0200

    Update script.js

--- lab2/script.js
+++ lab2/script.js
@@ -1,12 +1,11 @@
-(function(window) {
+(function() {
     var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
-
-    for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
+    names.forEach(function(name) {
+        var firstLetter = name.charAt(0).toLowerCase();
         if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]); // Виклик методу з SpeakGoodBye.js
+            byeSpeaker.speak(name);
         } else {
-            helloSpeaker.speak(names[i]); // Виклик методу з SpeakHello.js
+            helloSpeaker.speak(name);
         }
-    }
-})(window);
+    });
+})();
